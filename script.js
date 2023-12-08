1246cbc5e508d3b0e8edddaa2c1d4a4f1926189b
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2023-12-08 12:08:08 -0800

    added onclick attributes to index file

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-const calculator = {
-    displayValue: "0",
-    firstOperand: null,
-    waitForSecondOperand: false,
-    operator: null,
-};
+let initialValue = "";
 
-function inputDigit(digit) {
-    const { displayValue } = calculator;
-    calculator.displayValue = displayValue === "0" ? digit : displayValue + digit;
-
-function updateDisplay() {
-    const display = document.getElementById('display');
-    // updates value of displayValue
-    display.value = calculator.displayValue;
+function toDisplay(value) {
+    initialValue += value;
+    updateDisplay();
 }
 
-updateDisplay();
-
-const keys = document.querySelector('#calculator');
-keys.addEventListener('click' , (event) => {
-    const { target } = event;
-
-    if (!target.matches('button')) {
-        return;
-    }
-
-    if (target.classList.contains('oButton')) {
-        console.log('oButton', target.value);
-        return;
-    }
-
-    if (target.classList.contains('clear')) {
-        console.log('clear' , target.value);
-        return;
-    }
-
-    inputDigit(target.value);
+function clearDisplay() {
+    initialValue = '';
     updateDisplay();
-});
+}
